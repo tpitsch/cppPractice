78ac7be77990f19db48d2dac2d6124d7fc450c6c
Tyler <tpitsch@gmail.com>
2018-06-07 14:14:47 -0700

    init

---
+++ Project1/Project1/learingCPP.cpp
@@ -0,0 +1,20 @@
+#include "learingCPP.h"
+#include <iostream>
+
+
+learingCPP::learingCPP()
+{
+}
+
+
+learingCPP::~learingCPP()
+{
+}
+
+
+int main() {
+
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
---
+++ Project1/Project1/learingCPP.h
@@ -0,0 +1,8 @@
+#pragma once
+class learingCPP
+{
+public:
+	learingCPP();
+	~learingCPP();
+};
+
